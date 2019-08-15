8cb430450921ec8f629d4181db9a54e01d8a1d84
KiszonyKacper <38114155+KiszonyKacper@users.noreply.github.com>
2019-08-15 18:57:07 +0200

    Added antd along with babel import plugin

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import './App.css';
+
 import InitialPage from './components/InitialPage';
+
 
 import FeatureState from './context/feature/FeatureState';
 
--- src/App/components/InitialPage.js
+++ src/App/components/InitialPage.js
@@ -1,12 +1,22 @@
 import React, {useContext} from 'react';
+import { Layout, Button } from 'antd';
+
 import FeatureContext from '../context/feature/featureContext';
+
+const { Header } = Layout;
 
 const InitialPage = props => {
     const featureContext = useContext(FeatureContext);
 
     const {items} = featureContext;
 
-    return items.map(item => <h1>{item}</h1>)
+    return (
+        <Layout>
+            <Header className='header'>
+                <h1 className='title'>First ant design stuff</h1>
+            </Header>
+        </Layout>
+    )
 }
 
 export default InitialPage;
