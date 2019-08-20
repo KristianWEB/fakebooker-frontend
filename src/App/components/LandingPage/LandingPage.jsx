b76584800af3081c192964961faa7cb8050891ae
quincyhill <51150704+quincyhill@users.noreply.github.com>
2019-08-20 15:34:38 -0500

    Updated Landing (#21)

--- src/App/components/LandingPage/LandingPage.jsx
+++ src/App/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
-import {
-  Row, Col, Tabs, Input, Button,
-} from 'antd';
+import React, { useContext } from "react";
+import { Row, Col, Tabs, Input, Button, Checkbox } from "antd";
 
 import FeatureContext from '../../context/feature/featureContext';
 import './LandingPage.css';
 
 const { TabPane } = Tabs;
+const projectColor = "#3B5999";
 
 const Auth = (props) => (
   <div
+    className="authentication"
     style={{
       display: 'flex',
       alignItems: 'center',
@@ -27,38 +27,46 @@ const Auth = (props) => (
           <div className="auth_display">
             <h1>Welcome Back</h1>
             <h3>Login to continue</h3>
-            <Input placeholder="Email" size="large" />
-            <Input.Password placeholder="Password" size="large" />
-            <Button type="primary" block style={{ height: '40px' }}>
-              <span style={{ fontSize: '16px' }}>Login</span>
+            <Input placeholder='Email' size='large' />
+            <Input.Password placeholder='Password' size='large' />
+            <Checkbox style={{ alignSelf: "left" }}>Rememeber Me</Checkbox>
+            <Button type='primary' block style={{ height: "40px", backgroundColor: projectColor, borderColor: projectColor }}>
+              <span style={{ fontSize: "16px" }}>Login</span>
             </Button>
+            <a style={{ color: projectColor }} href="#">Forgot Password?</a>
           </div>
         </TabPane>
-        <TabPane tab="Register" key="2">
-          <div className="auth_display">
-            <h1>Create your account</h1>
+        <TabPane tab='Register' key='2'>
+          <div className='auth_display'>
+            <h1>Create Your Account</h1>
             <h3>Register</h3>
-            <Input placeholder="Email" size="large" />
-            <Input.Password placeholder="New Password" size="large" />
-            <Input.Password placeholder="Retype Password" size="large" />
-            <Button type="primary" block style={{ height: '40px' }}>
-              <span style={{ fontSize: '16px' }}>Register</span>
+            <Input placeholder='Email' size='large' />
+            <Input.Password placeholder='New Password' size='large' />
+            <Input.Password placeholder='Confirm Password' size='large' />
+            <Button type='primary' block style={{ height: "40px", backgroundColor: projectColor, borderColor: projectColor }}>
+              <span style={{ fontSize: "16px" }}>Register</span>
             </Button>
           </div>
         </TabPane>
       </Tabs>
     </Col>
-  </div>
+  </div >
 );
 
-const LandingPage = (props) => {
+const LandingContent = props => (
+  <div className="container">
+    <img src="https://www.123freevectors.com/wp-content/original/131220-dark-blue-polygon-background-graphic-design.jpg" alt="" />
+  </div>
+)
+
+const LandingPage = props => {
   const featureContext = useContext(FeatureContext);
   // const { items } = featureContext;
 
   return (
     <Row>
-      <Col span={16} style={{ backgroundColor: '#FEB048', height: '100vh' }}>
-        <h1>Left side</h1>
+      <Col span={16} style={{ backgroundColor: projectColor, height: "100vh" }}>
+        <LandingContent />
       </Col>
       <Col span={8} style={{ backgroundColor: '#FEFDF9', height: '100vh' }}>
         <Auth />
