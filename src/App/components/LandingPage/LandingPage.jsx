b01de6501c729c37c8fc46058d0d8e01d777b31e
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-08-19 22:35:10 -0400

    fixed travis build error

--- src/App/components/LandingPage/LandingPage.jsx
+++ src/App/components/LandingPage/LandingPage.jsx
@@ -3,48 +3,44 @@ import {
   Row, Col, Tabs, Input, Button,
 } from 'antd';
 
-
 import FeatureContext from '../../context/feature/featureContext';
 import './LandingPage.css';
 
 const { TabPane } = Tabs;
 
 
-const Auth = (props) => {
-  const { ok } = props;
-  return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-    }}
-    >
-      <Col span={20} offset={2}>
-        <Tabs tabPosition="top">
-          <TabPane tab="Login" key="1">
-            <div className="auth_display">
-              <h2>Login to continue</h2>
-              <Input placeholder="Email" size="large" />
-              <Input.Password placeholder="Password" size="large" />
-              <Button type="primary" block style={{ height: '40px' }}>
-                <span style={{ fontSize: '16px' }}>Login</span>
-              </Button>
-            </div>
-          </TabPane>
-          <TabPane tab="Register" key="2">
-            <div>
-              <h3>Register</h3>
-              <Input placeholder="Email" size="large" />
-              <Input.Password placeholder="Password" size="large" />
-              <Button type="primary" block>Login</Button>
-            </div>
-          </TabPane>
-        </Tabs>
-      </Col>
-    </div>
-  );
-};
+const Auth = (props) => (
+  <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  }}
+  >
+    <Col span={20} offset={2}>
+      <Tabs tabPosition="top">
+        <TabPane tab="Login" key="1">
+          <div className="auth_display">
+            <h2>Login to continue</h2>
+            <Input placeholder="Email" size="large" />
+            <Input.Password placeholder="Password" size="large" />
+            <Button type="primary" block style={{ height: '40px' }}>
+              <span style={{ fontSize: '16px' }}>Login</span>
+            </Button>
+          </div>
+        </TabPane>
+        <TabPane tab="Register" key="2">
+          <div>
+            <h3>Register</h3>
+            <Input placeholder="Email" size="large" />
+            <Input.Password placeholder="Password" size="large" />
+            <Button type="primary" block>Login</Button>
+          </div>
+        </TabPane>
+      </Tabs>
+    </Col>
+  </div>
+);
 
 const LandingPage = (props) => {
   const featureContext = useContext(FeatureContext);
