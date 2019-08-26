ef917d36cd4f3243ae5533c0b83b56ce3b011570
Foysal Ahmed Emon <emon535@gmail.com>
2019-08-26 22:10:57 +0600

    Created Gallery component folder (#30)
    * Created Gallery component and setup with router.
    
    * lint fixed

--- src/App/components/routing/Routes.jsx
+++ src/App/components/routing/Routes.jsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "../LandingPage/LandingPage";
 import NotFoundPage from "../NotFoundPage/NotFoundPage";
 import ProfilePage from "../ProfilePage/ProfilePage";
+import Gallery from "../Gallery/Gallery";
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/gallery" component={Gallery} />
         {/* below line for testing */}
         <Route component={NotFoundPage} />
       </Switch>
