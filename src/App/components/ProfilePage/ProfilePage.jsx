36f1f5b721e74692bc5d7857ac510806b9a4f5bf
Kristian <46797006+KristianWEB@users.noreply.github.com>
2019-09-17 01:57:58 +0300

    Feature/post integration (#51)
    * deleted package-lock
    
    * Post React Element Implemented

--- src/App/components/ProfilePage/ProfilePage.jsx
+++ src/App/components/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Post from "./components/Post";
 import ProfileHeader from "./components/ProfileHeader";
 
 export default function ProfilePage() {
   return (
     <div>
+      <Post />
       {/* TODO: Connect ProfileHeader to user */}
       <ProfileHeader
         coverImage="https://www.w3schools.com/w3images/avatar2.png"
