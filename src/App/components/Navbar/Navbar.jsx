854d71e0cffe4edcf5e491cc75d42726dce3fba0
Kristian <46797006+KristianWEB@users.noreply.github.com>
2019-09-19 02:05:51 +0300

    Convert Css Files to styled component files  (#59)

--- src/App/components/Navbar/Navbar.jsx
+++ src/App/components/Navbar/Navbar.jsx
@@ -1,48 +1,51 @@
 import React from "react";
-import { Icon, Row, Col, Input } from "antd";
-import "./Navbar.css";
+import { Col, Input } from "antd";
+import {
+  NavContainer,
+  LogoContainer,
+  LogoImg,
+  LogoHeader,
+  SearchBarContainer,
+  ProfileContainer,
+  ProfileHeader,
+  ProfileAvatar,
+  ActionsRoute,
+  ActionsContainer,
+  ActionsIcon,
+  Arrow
+} from "./Navbar.styles";
 import logo from "../../../assets/logo.svg";
 import ProfilePictureSample from "../../../assets/images/ProfilePictureSample.jpg";
 
 export default function Navbar() {
   return (
-    <Row className="nav-container">
+    <NavContainer>
       <Col span={4}>
-        <div className="logo">
-          <img src={logo} alt="logo" className="logo-img" />
-          <h2 className="logo-header">Sidekick</h2>
-        </div>
+        <LogoContainer>
+          <LogoImg src={logo} alt="logo" />
+          <LogoHeader>Sidekick</LogoHeader>
+        </LogoContainer>
       </Col>
       <Col span={6}>
-        <div className="search-bar">
+        <SearchBarContainer>
           <Input placeholder="Search friends,photos,videos" />
-        </div>
+        </SearchBarContainer>
       </Col>
       <Col span={5}>
-        <div className="user-actions">
-          <a href="/" className="user-actions-home">
-            Home
-          </a>
-          <Icon type="user-add" style={{ fontSize: "32px", color: "white" }} />
-          <Icon type="message" style={{ fontSize: "32px", color: "white" }} />
-          <Icon
-            type="notification"
-            style={{ fontSize: "32px", color: "white" }}
-          />
-        </div>
+        <ActionsContainer>
+          <ActionsRoute href="/">Home</ActionsRoute>
+          <ActionsIcon type="user-add" />
+          <ActionsIcon type="message" />
+          <ActionsIcon type="notification" />
+        </ActionsContainer>
       </Col>
       <Col span={6}>
-        <div className="profile">
-          <img
-            src={ProfilePictureSample}
-            alt="Profile"
-            width="50"
-            height="50"
-          />
-          <h2 className="profile-header">Kristian</h2>
-          <Icon type="down" style={{ fontSize: "16px", color: "white" }} />
-        </div>
+        <ProfileContainer>
+          <ProfileAvatar src={ProfilePictureSample} alt="Profile" />
+          <ProfileHeader className="profile-header">Kristian</ProfileHeader>
+          <Arrow type="down" />
+        </ProfileContainer>
       </Col>
-    </Row>
+    </NavContainer>
   );
 }
