050277e4888143e54439db0955388434a3ffb8d0
Kristian <devWEBpk@outlook.com>
2019-09-30 00:05:21 +0300

    implemented navbar component

--- src/App/components/Navbar/Navbar.jsx
+++ src/App/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   NavContainer,
   LogoContainer,
+  CurrentPage,
   AppLogo,
   ActionsContainer,
   FRContainer,
@@ -15,7 +16,16 @@ import {
   NotificationsCount,
   SearchContainer,
   Search,
-  NavMenu
+  SearchBar,
+  NavMenu,
+  ProfileContainer,
+  UserContainer,
+  UserAvatar,
+  UserStatus,
+  NameContainer,
+  Name,
+  Prefix,
+  UserSettings
 } from "./Navbar.styles";
 import Logo from "../../../assets/logo.svg";
 import FriendReqIcon from "../../../assets/icons/happy-face-icon.svg";
@@ -23,6 +33,8 @@ import MessagesIcon from "../../../assets/icons/chat---messages-icon.svg";
 import NotifyIcon from "../../../assets/icons/thunder-icon.svg";
 import SearchIcon from "../../../assets/icons/magnifying-glass-icon.svg";
 import NavMenuBurger from "../../../assets/icons/menu-icon.svg";
+import UserPictureSample from "../../../assets/images/Post-profile1.jpg";
+import DropDownArrow from "../../../assets/icons/dropdown-arrow-icon.svg";
 
 export default function Navbar({ displayName }) {
   return (
@@ -30,6 +42,8 @@ export default function Navbar({ displayName }) {
       <LogoContainer>
         <AppLogo src={Logo} />
       </LogoContainer>
+      <CurrentPage>PROFILE PAGE</CurrentPage>
+      <SearchBar placeholder="Search" />
       <ActionsContainer>
         <FRContainer>
           <FriendRequests src={FriendReqIcon} />
@@ -48,6 +62,17 @@ export default function Navbar({ displayName }) {
         </SearchContainer>
       </ActionsContainer>
       <NavMenu src={NavMenuBurger} />
+      <ProfileContainer>
+        <UserContainer>
+          <UserAvatar size={42} src={UserPictureSample} />
+          <UserStatus online="on" />
+          <NameContainer>
+            <Name>{displayName}</Name>
+            <Prefix>SPACE COWBOY</Prefix>
+          </NameContainer>
+        </UserContainer>
+        <UserSettings src={DropDownArrow} />
+      </ProfileContainer>
     </NavContainer>
   );
 }
--- src/App/components/Navbar/Navbar.styles.js
+++ src/App/components/Navbar/Navbar.styles.js
@@ -1,41 +1,86 @@
 import styled from "styled-components";
-import { Row } from "antd";
+import { Row, Input, Avatar } from "antd";
 
 export const NavContainer = styled(Row)`
   background-color: #3e4257;
   width: 100%;
-  height: 12vh;
+  height: 4.375rem;
   display: flex;
   align-items: center;
+  font-family: Roboto;
 `;
 
 export const LogoContainer = styled.div`
   padding-left: 5px;
 `;
+export const CurrentPage = styled.h3`
+  display: none;
+
+  @media (min-width: 1000px) {
+    display: block;
+    min-width: 12%;
+    font-size: 0.875rem;
+    margin: 0 0 0 1rem;
+    text-align: center;
+    color: #ffffff;
+    font-weight: bold;
+  }
+`;
 
 export const AppLogo = styled.img`
-  width: 40px;
-  height: 40px;
+  width: 2.5rem;
+  height: 2.5rem;
+
+  @media (min-width: 1000px) {
+    width: 3.75rem;
+    height: 3.75rem;
+  }
 `;
 
 export const ActionsContainer = styled(Row)`
   display: flex;
-  width: 100%;
+  width: 80%;
   justify-content: center;
 
-  & > *:not(:last-child) {
-    padding-right: 1.25rem;
+  @media (max-width: 768px) {
+    & > *:not(:last-child) {
+      padding-right: 8%;
+    }
+  }
+
+  @media (min-width: 768px) {
+    justify-content: center;
+    width: 45%;
+    margin: 0 2% 0 4%;
+  }
+
+  @media (min-width: 1350px) {
+    justify-content: flex-end;
+    margin-right: 0;
+    margin-left: 20%;
   }
 `;
 
 export const FRContainer = styled(Row)`
   display: flex;
   justify-content: center;
+
+  @media (min-width: 768px) {
+    padding-right: 10%;
+  }
+
+  @media (min-width: 1000px) {
+    padding-right: 15%;
+  }
+
+  @media (min-width: 1350px) {
+    padding-right: 12%;
+  }
 `;
 
 export const FriendRequests = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 1.875rem;
+  height: 1.875rem;
 `;
 
 export const FriendRequestsCount = styled.h5`
@@ -51,11 +96,23 @@ export const FriendRequestsCount = styled.h5`
 export const MessagesContainer = styled(Row)`
   display: flex;
   justify-content: center;
+
+  @media (min-width: 768px) {
+    padding-right: 10%;
+  }
+
+  @media (min-width: 1000px) {
+    padding-right: 15%;
+  }
+
+  @media (min-width: 1350px) {
+    padding-right: 12%;
+  }
 `;
 
 export const Messages = styled.img`
-  width: 35px;
-  height: 35px;
+  width: 2.18rem;
+  height: 2.18rem;
 `;
 
 export const MessagesCount = styled.h5`
@@ -71,11 +128,15 @@ export const MessagesCount = styled.h5`
 export const NotificationsContainer = styled(Row)`
   display: flex;
   justify-items: center;
+
+  @media (min-width: 768px) {
+    padding-right: 0;
+  }
 `;
 
 export const Notifications = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 1.875rem;
+  height: 1.875rem;
 `;
 
 export const NotificationsCount = styled.h5`
@@ -91,12 +152,103 @@ export const NotificationsCount = styled.h5`
 export const SearchContainer = styled(Row)``;
 
 export const Search = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 1.875rem;
+  height: 1.875rem;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const SearchBar = styled(Input)`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+    margin-left: 1rem;
+    width: 70%;
+    border-radius: 1.375rem;
+
+    ::placeholder {
+      color: #444444;
+    }
+  }
+
+  @media (min-width: 1000px) {
+    max-width: 25%;
+  }
 `;
 
 export const NavMenu = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 1.875rem;
+  height: 1.875rem;
   margin: 1rem;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const ProfileContainer = styled(Row)`
+  display: none;
+  @media (min-width: 768px) {
+    display: flex;
+    width: 40%;
+    align-items: center;
+    justify-content: flex-end;
+    margin: 0 1rem 0 0;
+  }
+
+  @media (min-width: 1000px) {
+    margin: 0 2rem 0 0;
+    width: 30%;
+  }
+  @media (min-width: 1350px) {
+    margin: 0 5rem 0 0;
+    width: 45%;
+  }
+`;
+
+export const UserContainer = styled(Row)`
+  display: flex;
+`;
+
+export const UserAvatar = styled(Avatar)``;
+
+export const UserStatus = styled.span`
+  position: absolute;
+  margin-right: 0;
+  top: 3px;
+  left: 0;
+  border: 1px solid #3f4256;
+  width: 0.5rem;
+  height: 0.5rem;
+  background-color: ${props => (props.online === "on" ? "#32e4cd" : "#777777")};
+  border-radius: 100%;
+`;
+
+export const NameContainer = styled(Row)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 0.5rem;
+`;
+
+export const Name = styled.h3`
+  font-size: 1rem;
+  font-weight: bold;
+  line-height: 1;
+  color: #ffffff;
+  margin: 0;
+`;
+
+export const Prefix = styled.h3`
+  font-size: 0.7rem;
+  color: #8185a2;
+  margin: 0;
+`;
+
+export const UserSettings = styled.img`
+  margin-left: 0.93rem;
+  width: 0.625rem;
+  height: 0.625rem;
 `;
