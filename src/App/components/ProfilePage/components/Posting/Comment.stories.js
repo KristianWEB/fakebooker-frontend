d170c1688cba8bd7ef108b376b654084a94ff46b
Kristian <devWEBpk@outlook.com>
2019-12-06 01:19:50 +0200

    storybook not working for some reason, working on that

--- src/App/components/ProfilePage/components/Posting/Comment.stories.js
+++ src/App/components/ProfilePage/components/Posting/Comment.stories.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import UserAvatar from "../../../../../assets/images/n4Ngwvi7.jpg";
-import UserAvatar2 from "../../../../../assets/images/avatar2-sm.jpg";
-import UserAvatar3 from "../../../../../assets/images/avatar5-sm.jpg";
-import UserAvatar4 from "../../../../../assets/images/avatar8-sm.jpg";
 import CreateComment from "./CreateComment";
 import Comment from "./Comment";
 import CommentList from "./CommentList";
 
 const comment = {
-  userAvatar: UserAvatar,
-  userName: "James Spiegel",
+  author: {
+    username: "James Spiegel",
+    coverImage: "https://www.w3schools.com/w3images/avatar2.png"
+  },
   body: "thats a nice tshirt you got dude"
 };
 
@@ -39,9 +38,9 @@ export const post = [
 const longBody =
   "This is a really really really long comment and it should expand its container with it and create a perfect comment. This has to be fixed so if the comment is too long it should drop below on a new file. Not now tho lets implement it first and then go refactor.";
 
-storiesOf("Comment", module)
-  .addDecorator(story => <div style={{ padding: "3rem" }}>{story()}</div>)
-  .add("create", () => <CreateComment userAvatar={UserAvatar} />)
-  .add("view", () => <Comment comment={comment} />)
-  .add("long body", () => <Comment comment={{ ...comment, body: longBody }} />)
-  .add("list", () => <CommentList post={post} />);
+// storiesOf("Comment", module)
+//   .addDecorator(story => <div style={{ padding: "3rem" }}>{story()}</div>)
+//   .add("create", () => <CreateComment userAvatar={UserAvatar} />)
+//   .add("view", () => <Comment comment={comment} postId={post[0].id} />)
+//   .add("long body", () => <Comment comment={{ ...comment, body: longBody }} />)
+//   .add("list", () => <CommentList post={post} />);
