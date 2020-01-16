54b426235fb4ab3c10105039f24c3cef50e04488
Kristian <devWEBpk@outlook.com>
2020-01-16 19:41:51 +0200

    about form implemented

--- src/components/About/About.stories.js
+++ src/components/About/About.stories.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import About from "./About";
+import AboutForm from "./AboutForm";
 
 storiesOf("About", module)
   .addDecorator(story => <div style={{ margin: "3rem" }}>{story()}</div>)
-  .add("default", () => <About />);
+  .add("default", () => <About />)
+  .add("active", () => <AboutForm />);
