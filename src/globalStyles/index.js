805eab9085ef8176bca09bb8c4f56c1feac7b53c
Kristian <46797006+KristianWEB@users.noreply.github.com>
2020-02-01 21:32:01 +0200

    normalized styles, 1rem = 10px (#156)

---
+++ src/globalStyles/index.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
+    h1, h2, h3, h4, h5, h6 {
+        margin: 0;
+    }
+
+   html, body {
+    font-size: 62.5%;
+   } 
+`;
