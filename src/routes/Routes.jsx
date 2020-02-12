f0053efe5e334d4a01f5a23b85971f88943a1f0e
Kristian <devWEBpk@outlook.com>
2020-02-12 17:42:12 +0200

    fixed routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import LandingPage from "../pages/LandingPage/LandingPage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import ProfilePage from "../pages/ProfilePage/ProfilePage";
 import PrivateRoute from "./PrivateRoute";
 
-export default function Routes() {
+const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={LandingPage} />
         <PrivateRoute exact path="/profile" component={ProfilePage} />
-        <Route component={NotFoundPage} />
+        <Redirect from="/" to="/profile" />
       </Switch>
     </Router>
   );
-}
+};
+
+export default Routes;
